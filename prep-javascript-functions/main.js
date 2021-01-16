0e74cd32f625cb4c474533e115edea0ad034390a
Osama Khan <osamakhan@osamas-air.lan>
2021-01-16 12:57:38 -0800

    Added all eleven functions and console logs

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,65 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(4, 4));
+
+function convertHoursToMinutes(number) {
+  return number * 60;
+}
+
+console.log(convertHoursToMinutes(2));
+
+function personalizeGreeting(name) {
+  return 'Hello ' + name;
+}
+
+console.log(personalizeGreeting('Osama'));
+
+function returnDatatype(data) {
+  return typeof(data);
+}
+
+console.log(returnDatatype(9000));
+
+function addAndMultiplyBy5(num1, num2) {
+  return (num1 + num2) * 5;
+}
+
+console.log(addAndMultiplyBy5(5, 5));
+
+function multiplyAndDivideBy5(num1, num2) {
+  return (num1 * num2) / 5;
+}
+
+console.log(multiplyAndDivideBy5(2, 3));
+
+function subtractTwoNumbers(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(subtractTwoNumbers(20, 3));
+
+function getCircleCircumference(radius) {
+  return 2 * radius * 3.14159;
+}
+
+console.log(getCircleCircumference(5));
+
+function returnFullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+
+console.log(returnFullName("Osama", "Khan"));
+
+function cubeNumber(number) {
+  return number * number * number;
+}
+
+console.log(cubeNumber(5));
+
+function returnMathSentence(num1, num2) {
+  return "If you add " + num1 + " and " + num2 + " together you get " + (num1 + num2) + ".";
+}
+
+console.log(returnMathSentence(5, 4));
